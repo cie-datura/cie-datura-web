79e04d23a3c15fd527e8501ed3b3f4f21fe29c99
Louissnsn <sanson.louis@gmail.com>
2025-11-04 13:40:36 +0100

    added bouton to try some stuff from it

---
+++ app/bouton/ButtonSecondary.tsx
@@ -0,0 +1,17 @@
+import svgPaths from "./SvgPathSecondary";
+
+export default function ButtonSecondary() {
+  return (
+    <div className="css-j9f0op css-vf8mzy" data-name="button-secondary">
+      <div className="css-g12nv8 css-roiesn" data-name="Vector">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 80 80">
+            <path d={svgPaths.pb21c980} fill="var(--fill-0, #FADE71)" id="Vector" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-8jezf7 css-8zrmd9 css-szzkzx css-tzu0k9">
+      </div>
+    </div>
+  );
+}
